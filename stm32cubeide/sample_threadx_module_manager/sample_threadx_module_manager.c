1d1cee53a853a3559a5f17428cc8779d012c7253
Wen Wang <wangwen@microsoft.com>
2023-06-08 14:18:57 +0800

    1. change to -Og  2. use txm_module_manager_memory_load txm_module_manager_in_place_load

--- stm32cubeide/sample_threadx_module_manager/sample_threadx_module_manager.c
+++ stm32cubeide/sample_threadx_module_manager/sample_threadx_module_manager.c
@@ -87,10 +87,12 @@ void    module_manager_entry(ULONG thread_input)
 
 #if 1
     /* Load the module that is already there, in this example it is placed there by the multiple image download.  */
-    txm_module_manager_in_place_load(&my_module, "my module", (VOID *) 0x080F0000);
+    //txm_module_manager_in_place_load(&my_module, "my module", (VOID *) 0x080F0000);
+
+    txm_module_manager_memory_load(&my_module, "my module", (VOID *) 0x080f0000);
 
     /* Enable 128 byte read/write shared memory region at 0x20008000.  */
-  //  txm_module_manager_external_memory_enable(&my_module, (void *) 0x20008000, 128, TXM_MODULE_MANAGER_SHARED_ATTRIBUTE_WRITE);
+    txm_module_manager_external_memory_enable(&my_module, (void *) 0x20008000, 128, TXM_MODULE_MANAGER_SHARED_ATTRIBUTE_WRITE);
 
     /* Start the module.  */
     txm_module_manager_start(&my_module);
@@ -100,22 +102,21 @@ void    module_manager_entry(ULONG thread_input)
 #endif
 
     /* Stop the module.  */
-  //  txm_module_manager_stop(&my_module);
+    txm_module_manager_stop(&my_module);
 
     /* Unload the module.  */
-  //  txm_module_manager_unload(&my_module);
+    txm_module_manager_unload(&my_module);
 
     /* Load the module that is already there.  */
-  //  txm_module_manager_in_place_load(&my_module_2, "my module_2", (VOID *) 0x080E0000);
+    txm_module_manager_in_place_load(&my_module, "my module", (VOID *) 0x080f0000);
     
     /* Enable 128 byte read/write shared memory region at 0x20008000.  */
- //   txm_module_manager_external_memory_enable(&my_module, (void *) 0x20008000, 128, TXM_MODULE_MANAGER_SHARED_ATTRIBUTE_WRITE);
+    txm_module_manager_external_memory_enable(&my_module, (void *) 0x20008000, 128, TXM_MODULE_MANAGER_SHARED_ATTRIBUTE_WRITE);
 
 
     /* Start the module again.  */
-  //  txm_module_manager_start(&my_module_2);
+    txm_module_manager_start(&my_module);
 
-   //     printf(" manager buffer address: %p, buffer address via pBuffer: %p \n", buffer, pBuffer);
     /* Now just spin...  */
     while(1)
     {
